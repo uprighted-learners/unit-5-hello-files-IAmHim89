f4d8e4b5c367a67ffad3612a3363b0e6cf1e4d58
IAmHim89 <computertech0418@gmail.com>
2024-07-03 17:37:59 -0400

    Assignment 98% complete

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
+});
+
+app.use(express.static("public"));
+
+app.get((req, res) => {
+  res.sendFile(__dirname, "index.html");
 });
